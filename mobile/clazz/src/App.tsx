d1e1db95aa779405fee14dac1c7fc06acf014f7c
William Philippe <wplb@ic.ufal.br>
2020-02-10 15:42:30 -0300

    #1 Configurado

---
+++ mobile/clazz/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Text>teste</Text>
+    </>
+  );
+};
+
+export default App;
